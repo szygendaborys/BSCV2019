4e37632ca1ffcb733595e203e64a88021ad31c3b
Borys Szygenda <43183384+jhrwekuh@users.noreply.github.com>
2019-02-25 23:23:39 +0000

    added few functionalities
    added slide on scroll, slide on arrow keys and the background

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ $(document).ready(() => {
 
     let menuCounter = 0;
     let pageCounter = 1;
+    let linksNumber = $('.link');
+
+    let keySlideUsed = false;
+    let scrollSlideUsed = false;
+    let mouseFocus = false;
 
     $( "#burger" ).click(() => {
         if(menuCounter === 0) {
@@ -67,10 +72,78 @@ function compactChangePage(e) {
     }
 }
 
+function slidePageKey(e) {
+    let keyValue = e.keyCode;
+
+    if(keySlideUsed==false) {
+        $('.navigating-info__arrows').css('display','none');
+        keySlideUsed = true;
+    }
+
+    if(keyValue==40 && pageCounter < linksNumber.length) { // down arrow slide down
+        pageCounter++;
+        $('.'+(pageCounter-1)).slideToggle();
+        $('.'+pageCounter).hide().slideToggle();
+        $('#'+(pageCounter-1)+'li').removeClass('link-active');
+        $('#'+pageCounter+'li').addClass('link-active');
+    } else if (keyValue==38 && pageCounter > 1) { // arrup slide up
+        pageCounter--;
+        $('.'+(pageCounter+1)).slideToggle();
+        $('.'+pageCounter).hide().slideToggle();
+        $('#'+(pageCounter+1)+'li').removeClass('link-active');
+        $('#'+pageCounter+'li').addClass('link-active');
+    } else {
+        return;
+    }
+}
+
+function slidePageScroll() {
+        let delta;
+
+        if(mouseFocus == false) {
+            if (event.wheelDelta){
+                delta = event.wheelDelta;
+            }else{
+                delta = -1 * event.deltaY;
+            }
+
+            if(scrollSlideUsed==false) {
+                $('.navigating-info__scroll').css('display','none');
+                scrollSlideUsed = true;
+            }
+
+            if (delta < 0 && pageCounter < linksNumber.length){ // scrolldown slide down
+                pageCounter++;
+                $('.'+(pageCounter-1)).slideToggle();
+                $('.'+pageCounter).hide().slideToggle();
+                $('#'+(pageCounter-1)+'li').removeClass('link-active');
+                $('#'+pageCounter+'li').addClass('link-active');
+            }else if (delta > 0 && pageCounter > 1){ // scrollup slide up
+                pageCounter--;
+                $('.'+(pageCounter+1)).slideToggle();
+                $('.'+pageCounter).hide().slideToggle();
+                $('#'+(pageCounter+1)+'li').removeClass('link-active');
+                $('#'+pageCounter+'li').addClass('link-active');
+            } else {
+                return;
+            }
+        }
+}
+
 
 // custom functions end
     $('.link').click( (e) => changePage(e) );
     $('.compact-link').click( (e) => compactChangePage(e) );
+
+    $('.container-box').on("mouseenter", () => {
+        mouseFocus = true;
+    }).on("mouseleave", () => {
+        mouseFocus = false;
+    });
+    
+    document.addEventListener("keydown", (e) => slidePageKey(e));
+    document.addEventListener("wheel", () => slidePageScroll());
+
 
 });
 
