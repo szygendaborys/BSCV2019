e97fdc5ac3fa193708c165afda316836d1ae9e87
Borys Szygenda <43183384+jhrwekuh@users.noreply.github.com>
2019-03-01 23:45:18 +0000

    deleted comment section

--- mail.php
+++ mail.php
@@ -1,10 +1,14 @@
-<?php $name = $_POST['name'];
+<?php 
+
+if (isset($_POST["submit"])) {
+
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "szygenda.borys@gmail.com";
-$subject = "Contact Form from $name";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+}
 ?>
